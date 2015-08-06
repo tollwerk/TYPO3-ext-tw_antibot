cc7759989b247d7aa59479ab574fb9a4729e1188
Joschi Kuphal <joschi@kuphal.net>
2015-08-06 21:14:11 +0200

    Adapted IP address database type

--- ext_tables.sql
+++ ext_tables.sql
@@ -6,7 +6,7 @@ CREATE TABLE tx_twantibot_domain_model_ip (
 	uid int(11) NOT NULL auto_increment,
 	pid int(11) DEFAULT '0' NOT NULL,
 
-	ip varchar(255) DEFAULT '' NOT NULL,
+	ip varchar(15) DEFAULT '' NOT NULL,
 
 	tstamp int(11) unsigned DEFAULT '0' NOT NULL,
 	crdate int(11) unsigned DEFAULT '0' NOT NULL,
