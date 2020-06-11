f3c104b198682f243b84329a9863eb417a740f4a
Joschi Kuphal <joschi@tollwerk.de>
2020-06-11 13:40:20 +0200

    Remove use statement from ext_tables.php

--- ext_tables.php
+++ ext_tables.php
@@ -1,27 +1,25 @@
 <?php
 
-use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
-
 defined('TYPO3_MODE') || die('Access denied.');
 
 call_user_func(
     function() {
-        ExtensionManagementUtility::addStaticFile(
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
             'tw_antibot',
             'Configuration/TypoScript/Static',
             'tollwerk Antibot'
         );
 
-        ExtensionManagementUtility::addLLrefForTCAdescr(
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
             'tx_twantibot_domain_model_blacklist',
             'EXT:tw_antibot/Resources/Private/Language/locallang_csh_tx_twantibot_domain_model_blacklist.xlf'
         );
-        ExtensionManagementUtility::allowTableOnStandardPages('tx_twantibot_domain_model_blacklist');
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_twantibot_domain_model_blacklist');
 
-        ExtensionManagementUtility::addLLrefForTCAdescr(
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
             'tx_twantibot_domain_model_whitelist',
             'EXT:tw_antibot/Resources/Private/Language/locallang_csh_tx_twantibot_domain_model_whitelist.xlf'
         );
-        ExtensionManagementUtility::allowTableOnStandardPages('tx_twantibot_domain_model_whitelist');
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_twantibot_domain_model_whitelist');
     }
 );
